4b805870ce9711abae1944abb24a93fdf06276c0
carlos Villermosa <carlos.viller98@gmail.com>
2020-08-23 21:19:30 -0300

    finished

---
+++ assets/js/todolist.js
@@ -0,0 +1,17 @@
+
+$('ul').on('click','li', function(){
+  $(this).toggleClass('completado')
+})
+
+$('ul').on('click','span', function(event){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+})
+
+$('input[type="text"').keypress(function (e) { 
+  if (e.key == 'Enter') {
+    $('ul').append(`<li><span><i class='fa fa-trash' aria-hidden='true'></i></span>${$(this).val()}</li>`);
+  }
+});
